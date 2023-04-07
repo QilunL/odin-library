7a9602f5fa169361dd118277e5eba864aabcd3de
QilunL <liauqilun98@gmail.com>
2023-04-07 13:16:10 +0800

    Read buttons and remove button added

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const submitBook = document.getElementById('submit-book');
 const newBookButton = document.getElementById('new-book-button');
 const newBookForm = document.getElementById('new-book-form');
 const checkReadButton = document.getElementById('read');
+let bookCount = 0;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -24,14 +25,34 @@ function makeCard(book) {
   const pagesDiv = document.createElement('div');
   const readDiv = document.createElement('div');
   const readButton = document.createElement('button');
+  const unreadButton = document.createElement('button');
   const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove Book';
+  readButton.textContent = 'Read';
+  unreadButton.textContent = 'Unread';
+
+  bookDiv.setAttribute('data', `count : ${bookCount}`);
+  bookCount += 1;
 
   bookDiv.classList.add('book');
   titleDiv.classList.add('title');
   authorDiv.classList.add('author');
   pagesDiv.classList.add('pages');
   readButton.classList.add('read-button');
+  unreadButton.classList.add('unread-button');
   removeButton.classList.add('remove-button');
+
+  readButton.addEventListener('click', () => {
+    readButton.parentElement.read = 1;
+    readDiv.textContent = 'Status: Read';
+  });
+  unreadButton.addEventListener('click', () => {
+    unreadButton.parentElement.read = 0;
+    readDiv.textContent = 'Status: Unread';
+  });
+  removeButton.addEventListener('click', () => {
+    removeButton.parentElement.remove();
+  });
 
   titleDiv.textContent = book.title;
   authorDiv.textContent = book.author;
@@ -45,6 +66,9 @@ function makeCard(book) {
   bookDiv.appendChild(authorDiv);
   bookDiv.appendChild(pagesDiv);
   bookDiv.appendChild(readDiv);
+  bookDiv.appendChild(readButton);
+  bookDiv.appendChild(unreadButton);
+  bookDiv.appendChild(removeButton);
   bookContainer.appendChild(bookDiv);
 }
 
@@ -73,9 +97,9 @@ function handleForm(event) {
     read = 0;
   }
   let book = new Book(title, author, pages, read);
-  console.log(book);
   addBookToLibrary(book);
   populateLibrary();
+  newBookForm.reset();
 }
 
 // submitBook.addEventListener('submit', handleForm);
